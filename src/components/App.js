7f7e7a59c29307b70ac120facdb66d322b3470f4
mariaadobos <mariyadob@gmail.com>
2020-01-06 14:18:00 +0100

    add basic html and css

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,66 @@
 import React from 'react';
 import '../stylesheets/App.scss';
 
-function App() {
-  return (
-    <div>holi</div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drug1: '',
+      drug2: '',
+      result: ''
+    }
+    this.onSubmitHandler = this.onSubmitHandler.bind(this);
+    this.getInputValue = this.getInputValue.bind(this);
+    //this.onClickHandler = this.onClickHandler.bind(this);
+  }
+  onSubmitHandler = event => {
+    event.preventDefault();
+  }
+  getInputValue = event => {
+    let name = event.target.name;
+    let value = event.target.value;
+    this.setState({
+      [name]: value
+    })
+    console.log(this.state.drug1)
+    console.log(this.state.drug2)
+
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <header></header>
+        <main className='main'>
+          <form className='form' onSubmit={this.onSubmitHandler}>
+            <label>Introduzca los nombres de los medicamentos</label>
+            <input
+            className='input'
+            type='text'
+            name='drug1'
+            value={this.state.drug1}
+            onChange={this.getInputValue}
+            />
+            <input
+            className='input'
+            type='text'
+            name='drug2'
+            value={this.state.drug2}
+            onChange={this.getInputValue}
+            />
+            <button
+            type='submit'
+            onClick={this.onClickHandler}>
+              Comparar
+            </button>
+          </form>
+          <section>
+            <p>{this.state.result}</p>
+          </section>
+        </main>
+        <footer></footer>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
