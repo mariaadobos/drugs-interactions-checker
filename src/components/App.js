08c7b27b938974b607c8dc05039c6f0e64c30059
mariaadobos <mariyadob@gmail.com>
2020-01-07 19:05:41 +0100

    creat array for autocomplete suggestions

--- src/components/App.js
+++ src/components/App.js
@@ -8,20 +8,32 @@ class App extends React.Component {
     this.state = {
       drug1: '',
       drug2: '',
-      result: ''
+      result: '',
+      suggestion: []
     }
     this.onSubmitHandler = this.onSubmitHandler.bind(this);
     this.getInputValue = this.getInputValue.bind(this);
     this.onClickHandler = this.onClickHandler.bind(this);
     this.fetchCIMA = this.fetchCIMA.bind(this);
+    this.getData = this.getData.bind(this);
   }
   fetchCIMA = () => {
     const ENDPOINT = `https://cima.aemps.es/cima/rest/medicamentos?nombre=${this.state.drug1}`;
     fetch(ENDPOINT)
   .then(response => response.json())
-  .then(data => console.log(data.resultados[0].vtm.nombre))
+  .then(data => this.getData(data.resultados))
   }
-
+  getData (data) {
+    console.log(data[0].nombre)
+    const suggestionArray = []
+    for (let item of data){
+       suggestionArray.push(item.nombre)
+    }
+    console.log(suggestionArray)
+    this.setState({
+      suggestion: suggestionArray
+    })
+  }
   onSubmitHandler = event => {
     event.preventDefault();
   }
