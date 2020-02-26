6252ccc73668982e2270a7dcbdb83d2eb5d13c7f
mariaadobos <mariyadob@gmail.com>
2020-02-27 00:04:09 +0100

    add autocomplete and refactor code

---
+++ docs/precache-manifest.b3f6ed8c1bc8e0051e83802b1fbe8b43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "884eabd4c0bed07bdbefafc96c63347b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "306cc755df2808489ab8",
+    "url": "./static/css/main.8ad12bcb.chunk.css"
+  },
+  {
+    "revision": "e9b1a365794da3fff20c",
+    "url": "./static/js/2.575cf519.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.575cf519.chunk.js.LICENSE"
+  },
+  {
+    "revision": "306cc755df2808489ab8",
+    "url": "./static/js/main.09646f22.chunk.js"
+  },
+  {
+    "revision": "38e4f2ee8442d878e55d",
+    "url": "./static/js/runtime-main.9ec987be.js"
+  }
+]);
